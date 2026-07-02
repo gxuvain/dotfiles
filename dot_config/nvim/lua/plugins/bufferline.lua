d74c3dc22c62a98edf1764ee16ed5f08ae0f1385
Gauvain Palanga <gauvainpalanga@gmail.com>
2026-07-02 22:09:27 +0200

    fix: lsp config

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,5 @@
 return {
 	"akinsho/bufferline.nvim",
-	-- enabled = false,
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -38,6 +38,17 @@ return {
 				},
 				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 			})
+
+			vim.diagnostic.config({
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = " ",
+						[vim.diagnostic.severity.WARN] = " ",
+						[vim.diagnostic.severity.HINT] = "󰠠 ",
+						[vim.diagnostic.severity.INFO] = " ",
+					}
+				}
+			})
 		end
 	}
 }
