719da635e925c36ab017c699ea805b72035e19ea
Gauvain Palanga <gauvainpalanga@gmail.com>
2026-07-09 17:56:23 +0200

    fix: remove deno adapter

--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -1,24 +1,22 @@
 ---@diagnostic disable: missing-fields, assign-type-mismatch
 return {
-	"nvim-neotest/neotest",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"antoinemadec/FixCursorHold.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"marilari88/neotest-vitest",
-		"MarkEmmons/neotest-deno"
-	},
-	keys = {
-		{ "<leader>tr", "<cmd>Neotest run<cr>" },
-		{ "<leader>to", "<cmd>Neotest output<cr>" },
-		{ "<leader>ts", "<cmd>Neotest summary<cr>" },
-	},
-	config = function()
-		require("neotest").setup({
-			adapters = {
-				require("neotest-vitest"),
-				require("neotest-deno"),
-			},
-		})
-	end,
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "marilari88/neotest-vitest",
+  },
+  keys = {
+    { "<leader>tr", "<cmd>Neotest run<cr>" },
+    { "<leader>to", "<cmd>Neotest output<cr>" },
+    { "<leader>ts", "<cmd>Neotest summary<cr>" },
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-vitest"),
+      },
+    })
+  end,
 }
