ff740f2a9e893fd56725749d4b48ee82b8b28a07
g.palanga <g.palanga@technologyandstrategy.com>
2026-07-16 16:56:32 +0200

    fix: add auto cmd for eslint to work on windows

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,15 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.hl.on_yank()
-	end
+  callback = function()
+    vim.hl.on_yank()
+  end
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.ts", "*.tsx", "*.js", "*.jsx", "*.vue" },
+  callback = function()
+    vim.lsp.buf.code_action({
+      context = { only = { "source.fixAll.eslint" }, diagnostics = {} },
+      apply = true,
+    })
+  end,
 })
