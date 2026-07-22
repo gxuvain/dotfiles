8ee2af89561b00d6c6c1b6a6bd06195e0a551863
Gauvain Palanga <gauvainpalanga@gmail.com>
2026-07-22 23:36:57 +0200

    fix: fix neovim config

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,28 +1,28 @@
 return {
-	"mason-org/mason.nvim",
-	dependencies = {
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		local tools = {
-			"lua-language-server",
-			"typescript-language-server",
-			"json-lsp",
-			"tailwindcss-language-server",
-			"vue-language-server",
-			"deno",
-			"basedpyright",
-			"eslint-lsp",
-		}
+  "mason-org/mason.nvim",
+  dependencies = {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    local tools = {
+      "lua-language-server",
+      "typescript-language-server",
+      "json-lsp",
+      "tailwindcss-language-server",
+      "vue-language-server",
+      "deno",
+      "basedpyright",
+      "eslint-lsp"
+    }
 
-		if vim.fn.has("mac") == 1 then
-			vim.list_extend(tools, { "ocaml-lsp", "ocamlformat" })
-		end
+    if vim.fn.has("mac") == 1 then
+      vim.list_extend(tools, { "ocaml-lsp", "ocamlformat" })
+    end
 
-		require("mason").setup()
-		require("mason-tool-installer").setup({
-			ensure_installed = tools,
-			auto_update = false,
-		})
-	end,
+    require("mason").setup()
+    require("mason-tool-installer").setup({
+      ensure_installed = tools,
+      auto_update = false,
+    })
+  end,
 }
