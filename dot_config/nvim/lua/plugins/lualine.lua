1daf686f3512bfd38465566f152e7516b2b45f2b
Gauvain Palanga <gauvainpalanga@gmail.com>
2026-09-15 18:55:45 +0200

    chore: clean stuff

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -4,11 +4,18 @@ vim.pack.add({
 })
 
 require("lualine").setup({
+  options = {
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+  },
   sections = {
     lualine_b = {
       { "branch", icon = "" },
       { "diff" },
       { "diagnostics" }
-    }
+    },
+    lualine_x = { "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "" }
   }
 })
