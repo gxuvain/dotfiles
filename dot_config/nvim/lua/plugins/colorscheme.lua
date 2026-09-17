8b72a97bbb1c260ae756d1ca97bc633ab89508b1
Gauvain Palanga <gauvainpalanga@gmail.com>
2026-09-17 23:23:09 +0200

    chore: change colorscheme to catppuccin

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,5 @@
-vim.pack.add({ "https://github.com/folke/tokyonight.nvim.git" })
+vim.pack.add({ "https://github.com/catppuccin/nvim" })
 
-require("tokyonight").setup({
-  on_colors = function(colors)
-    colors.bg = "#0b0c10"
-    colors.bg_dark = "#08090c"
-    colors.bg_float = "#0b0c10"
-    colors.bg_popup = "#0b0c10"
-    colors.bg_sidebar = "#090a0d"
-    colors.bg_statusline = "#090a0d"
-  end,
-})
+require("catppuccin").setup()
 
-vim.cmd.colorscheme("tokyonight-night")
+vim.cmd.colorscheme("catppuccin-mocha")
